629d45e137f752c08cf9d1b1bfe247e6efe92cbe
Igor <zaplatynskyy@gmail.com>
2021-11-17 19:03:03 +0100

    inserimento box con js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,35 @@
 // con difficoltà 2 => tra 1 e 81
 // con difficoltà 3 => tra 1 e 49
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
+
+const button = document.querySelector('.btn');
+
+button.addEventListener("click",
+    function() {
+        const container = document.querySelector('.container_box');
+        const range = document.getElementById('level').value;
+        container.innerHTML = '';
+        
+        switch(range) {
+            case '1':
+                for(let i = 1; i <= 100; i++) {
+                    container.innerHTML += `<div class="box easy">${i}</div>`;
+                }
+
+                break;
+            case '2':
+                for(let i = 1; i <= 81; i++) {
+                    container.innerHTML += `<div class="box hard">${i}</div>`;
+                }    
+
+                break;
+            case '3':
+                for(let i = 1; i <= 49; i++) {
+                    container.innerHTML += `<div class="box extreme">${i}</div>`;
+                } 
+                break;
+          } 
+
+    }
+);
+
